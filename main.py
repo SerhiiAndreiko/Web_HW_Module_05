4082342e449e4f03fbe99ee5db6105d25cef9f70
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2023-10-01 23:46:26 +0200

    update

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ import logging
 import json
 import argparse
 import asyncio
-from request_privat import main_request  # Assuming this is a module you've defined
+from request_privat import main_request  
 import pathlib
 
 BASE_DIR = pathlib.Path()
 
-async def main():
-    parser = argparse.ArgumentParser(description="Exchange Rate Information")
-    parser.add_argument('currency', help="Currency code")
-    parser.add_argument('cur_1', nargs='?', default="EUR", help="First currency code")
-    parser.add_argument('cur_2', nargs='?', default="USD", help="Second currency code")
-
-    args = parser.parse_args()
-
-    request = await main_request(args.currency)
+async def main(days, currency, cur_1="EUR", cur_2="USD"):
+    request = await main_request(currency)
 
     if request:
         with open(BASE_DIR.joinpath('data.json'), 'r', encoding='utf-8') as f:
@@ -24,24 +17,24 @@ async def main():
             with open(BASE_DIR.joinpath('cur_list.json'), 'r', encoding='utf-8') as f_cur_list:
                 d_cur_list = json.load(f_cur_list)
 
-                if args.cur_1 in d_cur_list and args.cur_2 in d_cur_list:
+                if cur_1 in d_cur_list and cur_2 in d_cur_list:
                     result = []
 
-                    for elem in d:
+                    for elem in d[:days]:
                         dt = elem.get('date')
                         res_dict = {dt: {}}
 
                         for rate_elem in elem.get('exchangeRate'):
                             currency = rate_elem.get('currency')
 
-                            if currency == args.cur_1:
-                                res_dict[dt][args.cur_1] = {
+                            if currency == cur_1:
+                                res_dict[dt][cur_1] = {
                                     'sale': rate_elem.get('saleRate'),
                                     'purchase': rate_elem.get('purchaseRate')
                                 }
 
-                            if currency == args.cur_2:
-                                res_dict[dt][args.cur_2] = {
+                            if currency == cur_2:
+                                res_dict[dt][cur_2] = {
                                     'sale': rate_elem.get('saleRate'),
                                     'purchase': rate_elem.get('purchaseRate')
                                 }
@@ -52,5 +45,16 @@ async def main():
                         json.dump(result, fd, ensure_ascii=False, indent=5)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description="Exchange Rate Information")
+    parser.add_argument('days', type=int, help="Number of days (up to 10)")
+    parser.add_argument('currency', help="Currency code")
+    parser.add_argument('cur_1', nargs='?', default="EUR", help="First currency code")
+    parser.add_argument('cur_2', nargs='?', default="USD", help="Second currency code")
+
+    args = parser.parse_args()
+
+    if args.days < 1 or args.days > 10:
+        print("Кількість днів повинна бути від 1 до 10.")
+    else:
+        logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
+        asyncio.run(main(args.days, args.currency, args.cur_1, args.cur_2))
